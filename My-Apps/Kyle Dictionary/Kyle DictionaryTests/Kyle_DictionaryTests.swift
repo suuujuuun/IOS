c0e44186d751ad5e0c7b3c56bb56b7d66cba4a25
Sjun <leesjun63@gmail.com>
2025-07-05 17:17:01 +0900

    add folder and clear files

---
+++ My-Apps/Kyle Dictionary/Kyle DictionaryTests/Kyle_DictionaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  Kyle_DictionaryTests.swift
+//  Kyle DictionaryTests
+//
+//  Created by 이승준 on 7/5/25.
+//
+
+import Testing
+@testable import Kyle_Dictionary
+
+struct Kyle_DictionaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ My-Apps/README.md
@@ -0,0 +1,4 @@
+# My apps
+
+Here are many apps, including apps that are not released in the App Store.
+
